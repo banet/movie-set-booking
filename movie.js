15081f407c5ba426b3f7a57676e76e612d0e6096
bane <bane.tepavcevic@gmail.com>
2020-07-31 09:18:05 +0200

    Implemented basic functionality

--- movie.js
+++ movie.js
@@ -3,22 +3,50 @@ const seat = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.querySelector("#movie");
+/**Selecting felt with movies */
+//const movieSelect = document.querySelector("#movie");
+//const ticketSelect = movieSelect.value;
+//Turning to the number two ways:
+// const ticketSelect = parseInt(movieSelect.value);, +movieSelect.value
 
 // Select film
+let ticketPrice = +movieSelect.value;
 
-const ticketPrice = +movieSelect.value;
-console.log(typeof ticketPrice);
-// Add event listener
-//  When I want to click on seats I want ot change the vclass to another ligt blue color
-container.addEventListener("click", seatFunction);
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  //console.log(selectedSeatsCount);
+  // console.log(selectedSeats);
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+//Choose movie
+function chooseMovie(e) {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+}
 
 function seatFunction(e) {
   // When you click , will show you which element you clicked!
   //console.log(e.target);
+  //if you have the class "seat" and it is NOT occupead than run toggle('selected')
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("occupied")
   ) {
     e.target.classList.toggle("selected");
   }
+  // Then go and update selected count
+  updateSelectedCount();
 }
+// Update the count and price
+
+// ADD EVENT LISTENER
+// Seat  click event
+//  When I want to click on seats I want ot change the vclass to another ligt blue color
+container.addEventListener("click", seatFunction);
+//container.addEventListener("click", updateSelectedCount);
+// Movie select event
+movieSelect.addEventListener("change", chooseMovie);
